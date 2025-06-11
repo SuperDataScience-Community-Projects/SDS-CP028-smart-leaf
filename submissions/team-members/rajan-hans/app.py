fa509b4235129097d9879565dc84652d65a343d4
rajanhans <rajan_hans@hotmail.com>
2025-06-11 11:24:42 -0700

    final upload

--- submissions/team-members/rajan-hans/app.py
+++ submissions/team-members/rajan-hans/app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import torch
+
 
 class_names = np.load('class_names.npy', allow_pickle=True).tolist()
 
 # --- Configuration ---
+#MODEL_PATH = 'efficientnet_best_model.keras'  # Path to your saved model
 MODEL_PATH = 'best_model.keras'  # Path to your saved model
+
 IMG_HEIGHT = 224
 IMG_WIDTH = 224
 
@@ -34,7 +38,7 @@ uploaded_file = st.file_uploader("Choose a crop leaf image...", type=["jpg", "jp
 if uploaded_file is not None:
     # Load and preprocess the image
     image = Image.open(uploaded_file).convert('RGB')
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     img = image.resize((IMG_HEIGHT, IMG_WIDTH))
     img_array = np.array(img)
